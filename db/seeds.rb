9cb1851b0d1cd3dc1212281b9474c5c366edd54f
devWRM <mcwen@comcast.net>
2021-05-27 06:10:21 -0400

    created 3 Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+# abe = User.create(name: 'Abraham', email: 'abe@email.com', password: 'abe')
+
+users = User.create([{name: "Abraham", email: "abe@email.com", password: "abe"}, {name: "Beatrice", email: "bea@email.com", password: "bea"}, {name: "Cecelia", email: "cc@email.com", password: "cc"}])
