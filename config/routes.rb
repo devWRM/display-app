ae1c5351e439c8f4f652025e0abe0cbed83d0fc1
devWRM <mcwen@comcast.net>
2021-05-28 03:06:18 -0400

    nested schedules in cleanings & users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   resources :schedules
-  resources :cleanings
-  resources :users
+
+  resources :cleanings do
+    resources :schedules
+  end
+
+  resources :users do
+    resources :schedules
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
