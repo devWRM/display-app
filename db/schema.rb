cb72f93d8cf4f8eff2cdd3f9ef3f900254dc1159
devWRM <mcwen@comcast.net>
2021-05-29 22:49:39 -0400

    created & migrated Status table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_27_134736) do
+ActiveRecord::Schema.define(version: 2021_05_30_024744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(version: 2021_05_27_134736) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["cleaning_id"], name: "index_schedules_on_cleaning_id"
     t.index ["user_id"], name: "index_schedules_on_user_id"
+  end
+
+  create_table "statuses", force: :cascade do |t|
+    t.boolean "active"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
