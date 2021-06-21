905feea29bc68490e81bef7df3903b8acddef762
devWRM <mcwen@comcast.net>
2021-06-21 19:13:06 -0400

    replaced cleanings with types

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :active
   has_many :schedules
-  has_many :cleanings, through: :schedules
+  has_many :types, through: :schedules
   has_many :rooms, through: :schedules
 end
