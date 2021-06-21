23c6894df5249e55c0e0e712d8d713f16b817390
devWRM <mcwen@comcast.net>
2021-06-21 18:45:48 -0400

    refactored to replace cleanings with types

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     # belongs_to :status
 
     has_many :schedules
-    has_many :cleanings, through: :schedules
+    has_many :types, through: :schedules
     has_many :rooms, through: :schedules
 
 
