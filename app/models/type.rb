29a48ceb2a124044ab6571ac24b089c1e2865c1c
devWRM <mcwen@comcast.net>
2021-06-21 18:39:41 -0400

    refactored all has many & has many through relationships

--- app/models/type.rb
+++ app/models/type.rb
@@ -1,2 +1,11 @@
 class Type < ApplicationRecord
+    has_many :schedules
+    has_many :assignments
+
+    has_many :users, through: :schedules
+    has_many :rooms, through: :schedules
+    has_many :cleanings, through: :assignments
+
+
+
 end
