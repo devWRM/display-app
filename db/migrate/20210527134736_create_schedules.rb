4a0d0797bc6d37d83ac2607aaaca7a9319af286f
devWRM <mcwen@comcast.net>
2021-06-21 16:01:08 -0400

    changed cleanings to type belongs to & included isActive boolean

--- db/migrate/20210527134736_create_schedules.rb
+++ db/migrate/20210527134736_create_schedules.rb
@@ -2,9 +2,11 @@ class CreateSchedules < ActiveRecord::Migration[6.0]
   def change
     create_table :schedules do |t|
       t.belongs_to :user, null: false, foreign_key: true
-      t.belongs_to :cleaning, null: false, foreign_key: true
+      t.belongs_to :class, null: false, foreign_key: true
+      t.belongs_to :room, null: false, foreign_key: true
       t.datetime :dated
       t.boolean :original
+      t.boolean :isActive, default: true
 
       t.timestamps
     end
