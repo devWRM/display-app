12ae7c66d4ccb7d93b002b491a9327a5a68bc3ac
devWRM <mcwen@comcast.net>
2021-06-21 15:59:06 -0400

    reworked isActive into migration and changed action to duty

--- db/migrate/20210527132702_create_cleanings.rb
+++ db/migrate/20210527132702_create_cleanings.rb
@@ -1,7 +1,9 @@
 class CreateCleanings < ActiveRecord::Migration[6.0]
   def change
     create_table :cleanings do |t|
-      t.string :action
+      t.string :duty
+      t.boolean :pass
+      t.boolean :isActive, default: true
 
       t.timestamps
     end
