8cc5e1412c01d264c43eab93854c812c683d21ac
devWRM <mcwen@comcast.net>
2021-06-24 13:01:09 -0400

    commented out code testing

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -3,4 +3,6 @@ class UserSerializer < ActiveModel::Serializer
   has_many :schedules
   has_many :types, through: :schedules
   has_many :rooms, through: :schedules
+
+  # has_many :assignments, through: :schedules, 
 end
