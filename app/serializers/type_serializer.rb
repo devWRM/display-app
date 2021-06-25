f6217db977dd81565661a73edf5243d73bc895c3
devWRM <mcwen@comcast.net>
2021-06-24 22:52:36 -0400

    added has many & through relationships for schedules & assignments

--- app/serializers/type_serializer.rb
+++ app/serializers/type_serializer.rb
@@ -1,5 +1,10 @@
 class TypeSerializer < ActiveModel::Serializer
   attributes :id, :name, :isActive
+
+  has_many :schedules
+  has_many :users, through: :schedules
+  has_many :rooms, through: :schedules
+
   has_many :assignments
   has_many :cleanings, through: :assignments
 end
