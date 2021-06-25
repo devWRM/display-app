325fd80a17f3541bac95036294e1d631ae08ba51
devWRM <mcwen@comcast.net>
2021-06-24 22:53:24 -0400

    added has many and has many through relationships

--- app/serializers/room_serializer.rb
+++ app/serializers/room_serializer.rb
@@ -1,4 +1,6 @@
 class RoomSerializer < ActiveModel::Serializer
   attributes :id, :name, :isActive
-  
+  has_many :schedules
+  has_many :users, through: :schedules
+  has_many :types, through: :schedules
 end
